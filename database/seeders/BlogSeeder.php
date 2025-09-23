a31d17da8dabe28ce2509c4dd9bea9b66603858e
GoNEAR <cagatay.morgul@outlook.com>
2025-09-23 11:20:17 +0200

    ajout path image dans le seeder blog

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Blog;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \Illuminate\Support\Facades\Storage;
 
 class BlogSeeder extends Seeder
 {
@@ -13,45 +14,49 @@ class BlogSeeder extends Seeder
      */
     public function run(): void
     {
+        // Récupérer toutes les images du dossier public/blog (disque public)
+        $images = Storage::disk('public')->allFiles('blog');
+
         $blogs = [
             [
                 'title' => 'Voyage au Japon',
                 'description' => 'Découvrez la culture japonaise à travers un voyage inoubliable.',
-                'image' => 'blog_japan.jpg',
+                'image' => $images[0],
                 'category_id' => 1,
                 'user_id' => 1,
             ],
             [
                 'title' => 'Bien-être et santé',
                 'description' => 'Conseils pour une vie saine et équilibrée.',
-                'image' => 'blog_health.jpg',
+                'image' => $images[1],
                 'category_id' => 2,
                 'user_id' => 1,
             ],
             [
                 'title' => 'Découvertes insolites',
                 'description' => 'Explorez des lieux et des faits méconnus.',
-                'image' => 'blog_discover.jpg',
+                'image' => $images[2],
                 'category_id' => 3,
                 'user_id' => 2,
             ],
             [
                 'title' => 'Tendances mode 2025',
                 'description' => 'Les dernières tendances fashion à ne pas manquer.',
-                'image' => 'blog_fashion.jpg',
+                'image' => $images[3],
                 'category_id' => 4,
                 'user_id' => 2,
             ],
             [
                 'title' => 'Entreprendre en 2025',
                 'description' => 'Astuces et conseils pour lancer son business.',
-                'image' => 'blog_business.jpg',
+                'image' => $images[4],
                 'category_id' => 5,
                 'user_id' => 1,
             ],
         ];
 
         foreach ($blogs as $b) {
+
             Blog::create($b);
         }
     }
